abbdf7da3fc056143b7ca816adb1bd7e1785d4d7
perecastor <archer.tb@hotmail.fr>
2012-12-07 17:02:42 +0100

    fond, perso qui bouge et qui change de skin ,point de vie

--- HoriZon/HoriZon/HoriZon/Game1.cs
+++ HoriZon/HoriZon/HoriZon/Game1.cs
@@ -19,6 +19,45 @@ namespace HoriZon
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        #region (variable skin)
+
+        private Texture2D skin_face;
+        private Texture2D skin_dos;
+        private Texture2D skin_droit;
+        private Texture2D skin_gauche;
+
+        #endregion (variable skin)
+
+        #region (variable fond)
+
+        private Texture2D pelouse;
+
+        #endregion (variable fond)
+
+        #region (variable position et deplacement du perso)
+
+        Vector2 position_perso;
+        Vector2 deplacement_perso;
+
+        #endregion (variable position et deplacement du perso)
+
+        #region (skin_perso)
+
+        Texture2D skin_perso;
+
+        #endregion (skin_perso)
+
+        #region (gameplay)
+
+        KeyboardState clavier;
+
+        int point_de_vie;
+        Vector2 position_point_de_vie;
+        SpriteFont police_point_de_vie;
+
+        #endregion (gameplay)
+
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -34,6 +73,10 @@ namespace HoriZon
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            point_de_vie = 100;
+            position_point_de_vie = new Vector2(2, 3);
+            position_perso = new Vector2(2, 3);
+            deplacement_perso = new Vector2(0, 0);
 
             base.Initialize();
         }
@@ -46,6 +89,18 @@ namespace HoriZon
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            skin_face = Content.Load<Texture2D>("skin_face");
+            skin_dos = Content.Load<Texture2D>("skin_dos");
+            skin_droit = Content.Load<Texture2D>("skin_droit");
+            skin_gauche = Content.Load<Texture2D>("skin_gauche");
+
+            //initialisation
+            skin_perso = Content.Load<Texture2D>("skin_dos");
+
+            pelouse = Content.Load<Texture2D>("pelouse");
+
+            police_point_de_vie = Content.Load<SpriteFont>("police_point_de_vie");
 
             // TODO: use this.Content to load your game content here
         }
@@ -71,6 +126,31 @@ namespace HoriZon
                 this.Exit();
 
             // TODO: Add your update logic here
+            clavier = Keyboard.GetState();
+
+            if (clavier.IsKeyDown(Keys.Up))
+            {
+                skin_perso = skin_dos;
+                deplacement_perso = new Vector2(0, -1);
+            }
+            else if (clavier.IsKeyDown(Keys.Down))
+            {
+                skin_perso = skin_face;
+                deplacement_perso = new Vector2(0, 1);
+            }
+
+            if (clavier.IsKeyDown(Keys.Right))
+            {
+                skin_perso = skin_droit;
+                deplacement_perso = new Vector2(1, 0);
+            }
+            else if (clavier.IsKeyDown(Keys.Left))
+            {
+                skin_perso = skin_gauche;
+                deplacement_perso = new Vector2(-1, 0);
+            }
+
+            position_perso = position_perso + deplacement_perso;
 
             base.Update(gameTime);
         }
@@ -84,6 +164,19 @@ namespace HoriZon
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+
+            #region(affichage_images)
+
+            spriteBatch.Begin();
+
+            // la ou on fou l'affichage des images
+            spriteBatch.Draw(pelouse, Vector2.Zero, Color.White);
+            spriteBatch.Draw(skin_perso, position_perso, Color.White);
+            spriteBatch.DrawString(police_point_de_vie, "point de vie " + point_de_vie, position_point_de_vie, Color.White);
+
+            spriteBatch.End();
+
+            #endregion(affichage_images)
 
             base.Draw(gameTime);
         }
