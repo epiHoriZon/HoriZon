5adcbd3811c79e18aeead442570d692baf3bd262
perecastor <archer.tb@hotmail.fr>
2012-12-08 10:26:51 +0100

    le menu fonctionne, il manque les options(et le menu qui va avec)

--- HoriZon/HoriZon/HoriZon/Game1.cs
+++ HoriZon/HoriZon/HoriZon/Game1.cs
@@ -30,7 +30,7 @@ namespace HoriZon
 
         #region (variable fond)
 
-        private Texture2D pelouse;
+        Texture2D pelouse;
 
         #endregion (variable fond)
 
@@ -57,6 +57,22 @@ namespace HoriZon
 
         #endregion (gameplay)
 
+        #region(menu)
+
+        bool menu_ou_pas;
+        bool enter_menu_ou_pas;
+        int compteur_menu;
+
+        Texture2D menu;
+
+        Texture2D jouer_menu;
+        Texture2D options_menu;
+        Texture2D credit_menu;
+        Texture2D quitter_menu;
+
+        Texture2D credit_menu_enter;
+
+        #endregion(menu)
 
         public Game1()
         {
@@ -73,8 +89,14 @@ namespace HoriZon
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+
+            enter_menu_ou_pas = false;
+            compteur_menu = 0;
+            menu_ou_pas = true;
+
             point_de_vie = 100;
             position_point_de_vie = new Vector2(2, 3);
+
             position_perso = new Vector2(2, 3);
             deplacement_perso = new Vector2(0, 0);
 
@@ -102,6 +124,16 @@ namespace HoriZon
 
             police_point_de_vie = Content.Load<SpriteFont>("police_point_de_vie");
 
+            jouer_menu = Content.Load<Texture2D>("jouer_menu");
+            options_menu = Content.Load<Texture2D>("options_menu");
+            credit_menu = Content.Load<Texture2D>("credit_menu");
+            quitter_menu = Content.Load<Texture2D>("quitter_menu");
+
+            credit_menu_enter = Content.Load<Texture2D>("credit_menu_enter");
+
+            //initialisation
+            menu = Content.Load<Texture2D>("jouer_menu");
+
             // TODO: use this.Content to load your game content here
         }
 
@@ -128,29 +160,94 @@ namespace HoriZon
             // TODO: Add your update logic here
             clavier = Keyboard.GetState();
 
-            if (clavier.IsKeyDown(Keys.Up))
+            if (menu_ou_pas)
             {
-                skin_perso = skin_dos;
-                deplacement_perso = new Vector2(0, -1);
-            }
-            else if (clavier.IsKeyDown(Keys.Down))
-            {
-                skin_perso = skin_face;
-                deplacement_perso = new Vector2(0, 1);
+                if (enter_menu_ou_pas)
+                {
+                    if (clavier.IsKeyDown(Keys.Back))
+                    {
+                        enter_menu_ou_pas = false;
+                    }
+                    else if (compteur_menu % 4 == 0)
+                    {
+                        enter_menu_ou_pas = false;
+                        menu_ou_pas = false;
+                    }
+                    else if (compteur_menu % 4 == 1)
+                    {
+                        //en attendant le menu options
+                        menu = credit_menu_enter;
+                    }
+                    else if (compteur_menu % 4 == 2)
+                    {
+                        menu = credit_menu_enter;
+                    }
+                    else if (compteur_menu % 4 == 3)
+                    {
+                        Exit();
+                    }
+                }
+                else
+                {
+                    if (clavier.IsKeyDown(Keys.Up))
+                    {
+                        compteur_menu = compteur_menu + 1;
+                    }
+                    else if (clavier.IsKeyDown(Keys.Down))
+                    {
+                        compteur_menu = compteur_menu + 1;
+                    }
+                    else if (clavier.IsKeyDown(Keys.Enter))
+                    {
+                        enter_menu_ou_pas = true;
+                    }
+
+                    if (compteur_menu % 4 == 0)
+                    {
+                        menu = jouer_menu;
+                    }
+                    else if (compteur_menu % 4 == 1)
+                    {
+                        menu = options_menu;
+                    }
+                    else if (compteur_menu % 4 == 2)
+                    {
+                        menu = credit_menu;
+                    }
+                    else if (compteur_menu % 4 == 3)
+                    {
+                        menu = quitter_menu;
+                    }
+                }
             }
 
-            if (clavier.IsKeyDown(Keys.Right))
+            else
             {
-                skin_perso = skin_droit;
-                deplacement_perso = new Vector2(1, 0);
-            }
-            else if (clavier.IsKeyDown(Keys.Left))
-            {
-                skin_perso = skin_gauche;
-                deplacement_perso = new Vector2(-1, 0);
-            }
 
-            position_perso = position_perso + deplacement_perso;
+                if (clavier.IsKeyDown(Keys.Up))
+                {
+                    skin_perso = skin_dos;
+                    deplacement_perso = new Vector2(0, -1);
+                }
+                else if (clavier.IsKeyDown(Keys.Down))
+                {
+                    skin_perso = skin_face;
+                    deplacement_perso = new Vector2(0, 1);
+                }
+
+                else if (clavier.IsKeyDown(Keys.Right))
+                {
+                    skin_perso = skin_droit;
+                    deplacement_perso = new Vector2(1, 0);
+                }
+                else if (clavier.IsKeyDown(Keys.Left))
+                {
+                    skin_perso = skin_gauche;
+                    deplacement_perso = new Vector2(-1, 0);
+                }
+
+                position_perso = position_perso + deplacement_perso;
+            }
 
             base.Update(gameTime);
         }
@@ -170,9 +267,17 @@ namespace HoriZon
             spriteBatch.Begin();
 
             // la ou on fou l'affichage des images
-            spriteBatch.Draw(pelouse, Vector2.Zero, Color.White);
-            spriteBatch.Draw(skin_perso, position_perso, Color.White);
-            spriteBatch.DrawString(police_point_de_vie, "point de vie " + point_de_vie, position_point_de_vie, Color.White);
+            if (menu_ou_pas)
+            {
+                spriteBatch.Draw(menu, Vector2.Zero, Color.White);
+            }
+
+            else
+            {
+                spriteBatch.Draw(pelouse, Vector2.Zero, Color.White);
+                spriteBatch.Draw(skin_perso, position_perso, Color.White);
+                spriteBatch.DrawString(police_point_de_vie, "point de vie " + point_de_vie, position_point_de_vie, Color.White);
+            }
 
             spriteBatch.End();
 
